e452c8710448d6ca1b55db8e4324063f60472ca7
johnmarklumapac <102014089+johnmarklumapac@users.noreply.github.com>
2023-05-18 17:24:22 +0800

    Debug False

--- core/urls.py
+++ core/urls.py
@@ -3,7 +3,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
-from django.urls import include, path
+from django.urls import include, path, re_path
 from django.views.static import serve
 
 urlpatterns = [
