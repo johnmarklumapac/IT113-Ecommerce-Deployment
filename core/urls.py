a335556675f5669cfd8500f2de969102f22c1e3f
johnmarklumapac <102014089+johnmarklumapac@users.noreply.github.com>
2023-05-18 18:10:38 +0800

    Update urls.py

--- core/urls.py
+++ core/urls.py
@@ -5,7 +5,6 @@ from django.contrib import admin
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.urls import include, path, re_path
 from django.views.static import serve
-from whitenoise import WhiteNoise
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -17,12 +16,11 @@ urlpatterns = [
     path("__debug__/", include(debug_toolbar.urls)),
 ]
 
-urlpatterns += [
-    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
-]
-
-if not settings.DEBUG:
-    # Serve static files via Whitenoise
+if settings.DEBUG:
+    urlpatterns += staticfiles_urlpatterns()
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+else:
     urlpatterns += [
-        re_path(r'^static/(?P<path>.*)$', WhiteNoise(settings.STATICFILES_DIRS), name='static'),
+        re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+        re_path(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
     ]
