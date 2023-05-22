9dfa756b054caac6da4fbc818c44406ecdc37677
johnmarklumapac <102014089+johnmarklumapac@users.noreply.github.com>
2023-05-22 11:33:35 +0800

    some changes

--- core/wsgi.py
+++ core/wsgi.py
@@ -1,15 +1,11 @@
-# This code is running on render finally
-
 import os
 
+from django.conf import settings
 from django.core.wsgi import get_wsgi_application
 from whitenoise import WhiteNoise
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings.base')
 application = get_wsgi_application()
-application = get_wsgi_application()
-
-from django.conf import settings
 
 settings.ROOT_URLCONF = 'core.urls'
 application = WhiteNoise(application)
